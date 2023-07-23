3f050f9c0a948ae43dbec84846d1e67a86f79007
Vamshi-Anantha <124914610+Vamshi-Anantha@users.noreply.github.com>
2023-07-23 23:35:46 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    static int maximumSumarray(int K, ArrayList<Integer> Arr, int N) {
+        /*
+         * int max = Integer.MIN_VALUE;
+         * 
+         * for(int i =0; i<N-K; i++)
+         * {
+         * int sum = 0;
+         * for (int j=0; j<K; j++)
+         * {
+         * sum = sum+Arr.get(i+j);
+         * }
+         * if(sum>max) max = sum;
+         * }
+         * return max;
+         */
+
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum = sum + Arr.get(i);
+        }
+        max = sum;
+        for (int i = 0; i < N - K; i++) {
+            sum = sum - Arr.get(i) + Arr.get(i + K);
+            if (sum > max)
+                max = sum;
+        }
+
+        return max;
+    }
+}
